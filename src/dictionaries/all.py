e4928f51dd15c3c1874e6d2d175bfc3e47495288
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-04-04 23:37:11 +0200

    Update all.py

--- src/dictionaries/all.py
+++ src/dictionaries/all.py
@@ -1,10 +1,12 @@
 import json
-import os, json
 from pathlib import Path
 
 HERE = Path(__file__).parent.resolve()
 
-dicts = {
-    path.stem: json.loads(path.read_text())
-    for path in HERE.glob("*.json")
-}
+dicts = {path.stem: json.loads(path.read_text()) for path in HERE.glob("*.json")}
+
+
+if __name__ == "__main__":
+    from rich import print
+
+    print(dicts)
